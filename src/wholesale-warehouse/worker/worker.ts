c61f6e766b9e780431c107aec5c1f6c0ad970d4d
Chen Osipov <eobender@gmail.com>
2024-10-01 18:57:19 -0700

    fix.

--- src/wholesale-warehouse/worker/worker.ts
+++ src/wholesale-warehouse/worker/worker.ts
@@ -1,34 +1,22 @@
-import { MemoryCache } from "../../utils/cashing/memoryCache"
-import { WHOLESALE_WAREHOUSE_GLUE } from "../../utils/extension/glue"
-import { SiteMetadata } from "../../utils/site/site-information"
-import { ParsedHtml } from "../../utils/dom/html"
-import { prepareDTO, preprocessAlibabaData } from "../stores/alibaba/alibaba-store"
-import { analyzeWholesaleWarehouseProductByAI } from "../wholesale-warehouse-ai-api-service"
-import { convertedAlibabaProduct } from "../mocks/alibaba-product-mock"
-
-const cache = new MemoryCache()
+import { MemoryCache } from "../../utils/cashing/memoryCache";
+import { WHOLESALE_WAREHOUSE_GLUE } from "../../utils/extension/glue";
+import { SiteMetadata } from "../../utils/site/site-information";
+import { ParsedHtml } from "../../utils/dom/html";
+import { prepareDTO, preprocessAlibabaData } from "../stores/alibaba/alibaba-store";
+import { analyzeWholesaleWarehouseProductByAI } from "../wholesale-warehouse-ai-api-service";
 
 export const initWholesaleWarehouse = () => {
-	WHOLESALE_WAREHOUSE_GLUE.worker(async (data: any, postMessage: any): Promise<void> => {
-		const dom: ParsedHtml = SiteMetadata.getDom(data)
-		const urlObj = data?.url
-		const wholesaleWhStoreData = preprocessAlibabaData(dom)
-		const wholesaleAiDTO = prepareDTO(wholesaleWhStoreData)
+  WHOLESALE_WAREHOUSE_GLUE.worker(async (data: any, postMessage: any): Promise<void> => {
+    const dom: ParsedHtml = SiteMetadata.getDom(data);
+    const urlObj = data?.url;
+    const wholesaleWhStoreData = preprocessAlibabaData(dom);
+    const wholesaleAiDTO = prepareDTO(wholesaleWhStoreData);
+    const aiResult = await analyzeWholesaleWarehouseProductByAI(wholesaleAiDTO);
+    const safeDealAiResult = convertedAlibabaProduct(aiResult, urlObj);
 
-		// ----------------------------
-		// Analyzes the product by AI/Mock
-		// ----------------------------
-		const aiResult = await analyzeWholesaleWarehouseProductByAI(wholesaleAiDTO)
-		console.log("AI Result: ", aiResult)
-		const safeDealAiResult = convertedAlibabaProduct(aiResult, urlObj)
-
-		// This if For Mock ONLY on Development Checking!
-		// const safeDealAiResult = await new Promise((resolve) => {
-		//   // resolve(alibabaProductMock);
-		//   // resolve(convertedAlibabaProductMock);
-		// });
-
-		console.log("Wholesale AI Result: ", safeDealAiResult)
-		postMessage(safeDealAiResult)
-	})
+    postMessage(safeDealAiResult);
+  });
+};
+function convertedAlibabaProduct(aiResult: any, urlObj: any) {
+  throw new Error("Function not implemented.");
 }

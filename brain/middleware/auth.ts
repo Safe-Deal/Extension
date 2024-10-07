d00e6ccd9dd6bfb94cf9428f2274d842d8e449a8
Chen Osipov <eobender@gmail.com>
2024-10-07 04:45:13 -0700

    Update verdict.ts, const.ts, constants.ts, scores.ts, type.ts, weight.ts, site-response.interface.ts, reviews-types.ts, walmart-utils.ts, rule-summary-tooltip.interface.ts, site-creator.interface.ts, rule.interface.ts, meta-data.interface.ts, rule-manager-result.interface.ts, svg.d.ts, interfaces.ts, IProductSelector.ts, emotion.ts, icons.ts, sites.ts, colors.ts, IProductListSelector.ts, and constants.ts

--- brain/middleware/auth.ts
+++
@@ -1,9 +0,0 @@
-const API_KEY = process.env.AUTH_API_KEY;
-
-export const apiAuth = () => (req, res, next) => {
-  if (req.headers["api-key"] !== API_KEY) {
-    res.status(403).send("Forbidden");
-  } else {
-    next();
-  }
-};
--- brain/services/scrapify/utils.ts
+++
@@ -1,3 +0,0 @@
-export function urlsafe_b64encode(data: string): string {
-  return Buffer.from(data, "utf-8").toString("base64").replace("+", "-").replace("/", "_").replace(/=+$/, "");
-}

0e6336814da9dbe618bd8840c1fe879e18c570ae
Safi-ur-Rehman <safiurrehman0@gmail.com>
2024-10-15 19:22:11 +0500

    feat: +Anti scam store, sw now using zustand and transport

--- src/anti-scam/anti-scam-worker.ts
+++ src/anti-scam/anti-scam-worker.ts
@@ -1,32 +1,66 @@
 import { debug, logError } from "../utils/analytics/logger";
 import { ext } from "../utils/extension/ext";
-import { ANTI_SCAM_GLUE } from "../utils/extension/glue";
 import { ApiScamPartners } from "./logic/anti-scam-logic";
+import { initAntiScamStoreBackend } from "@store/AntiScamState";
+import { definePegasusMessageBus } from "@utils/pegasus/transport";
 
 export const CLOSE_TAB = "close-tab";
 
-export const initAntiScamWorker = () => {
-  ANTI_SCAM_GLUE.worker((domain: string, sendResponse, sender) => {
-    if (domain === CLOSE_TAB) {
-      const tabId = sender.tab.id;
-      ext.tabs.remove(tabId, () => {
-        debug(`AntiScamWorker :: Closed dangerous tab ${tabId} `);
-      });
-      return;
-    }
+export enum AntiScamMessageTypes {
+  ANALYZE_DOMAIN = "analyzeDomain"
+}
+export interface IAntiScamMessageBus {
+  [AntiScamMessageTypes.ANALYZE_DOMAIN]: (domain: string) => Promise<void>;
+}
 
-    debug(`AntiScamWorker :: Examining ${domain} ...`);
-    const verifier = new ApiScamPartners();
-    verifier
-      .evaluate(domain, sender.tab.id)
-      .then((result) => {
-        debug(`AntiScamWorker :: ${domain} is ${JSON.stringify(result)}`);
-        sendResponse(result);
-        debug(`AntiScamWorker :: Examining ${domain} Response Sent.`);
-      })
-      .catch((error) => {
-        debug(`AntiScamWorker :: Error: ${error}`);
-        logError(error);
-      });
+export const initAntiScamWorker = async () => {
+  const store = await initAntiScamStoreBackend();
+  const { setConclusion, setLoading } = store.getState();
+  debug("AntiScamStore:: Anti Scam Store ready!");
+  const { onMessage } = definePegasusMessageBus<IAntiScamMessageBus>();
+
+  onMessage(AntiScamMessageTypes.ANALYZE_DOMAIN, async (request) => {
+    setLoading(true);
+
+    try {
+      if (!request || !request.data) {
+        throw new Error("Invalid request or missing data");
+      }
+
+      const domain = request.data;
+      const sender = request.sender;
+
+      if (!domain || !sender) {
+        throw new Error("Missing required data: url");
+      }
+      if (domain === CLOSE_TAB) {
+        const tabId = sender?.tabId;
+        ext.tabs.remove(tabId, () => {
+          debug(`AntiScamWorker :: Closed dangerous tab ${tabId} `);
+        });
+        return;
+      }
+
+      debug(`AntiScamWorker :: Examining ${domain}...`);
+
+      const verifier = new ApiScamPartners();
+      verifier
+        .evaluate(domain, sender?.tabId)
+        .then((result) => {
+          setConclusion(result);
+          debug(`AntiScamWorker :: ${domain} is ${JSON.stringify(result)}`);
+          debug(`AntiScamWorker :: Examining ${domain} Response Sent.`);
+          setLoading(false);
+        })
+        .catch((error) => {
+          debug(`AntiScamWorker :: Error: ${error}`);
+          logError(error);
+        });
+    } catch (error) {
+      debug(`AntiScamWorker :: Error: ${error}`);
+      logError(error, "AntiScamWorker :: Error:");
+    } finally {
+      setLoading(false);
+    }
   });
 };

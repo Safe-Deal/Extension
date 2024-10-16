87ac2082971d7a2b46ad2decb5995a88a737da8d
Safi-ur-Rehman <safiurrehman0@gmail.com>
2024-10-16 15:43:22 +0500

    feat: +Shutaf store, sw using transport and zustand

--- src/shutaf/content-script-shutaf.tsx
+++ src/shutaf/content-script-shutaf.tsx
@@ -1,24 +1,47 @@
 import { authStoreReady, useAuthStore } from "@store/AuthState";
+import { shutafStoreReady } from "@store/ShutafState";
+import { IShutafMessageBus, ShutafMessageType } from "./shutaf-worker";
 import { initPegasusTransport } from "@utils/pegasus/transport/content-script";
 import { debug, logError } from "../utils/analytics/logger";
 import { browserWindow } from "../utils/dom/html";
 import { onHrefChange } from "../utils/dom/location";
-import { SHUTAF_GLUE } from "../utils/extension/glue";
+import { definePegasusMessageBus } from "@utils/pegasus/transport";
 
 const PING_INTERVAL_IN_SEC = 25;
 
-initPegasusTransport();
+initPegasusTransport({ allowWindowMessagingForNamespace: "CONTENT_SCRIPT_SHUTAF" });
 
-const setupPing = () => {
-  setInterval(() => {
-    SHUTAF_GLUE.ping();
-  }, PING_INTERVAL_IN_SEC * 1000);
-};
+(async () => {
+  try {
+    await authStoreReady();
+    await shutafStoreReady();
+    const { user } = useAuthStore.getState();
+    const userMetadata = user?.user_metadata;
+    const { sendMessage } = definePegasusMessageBus<IShutafMessageBus>();
 
-const storeAndSendUrl = (url: string) => {
-  SHUTAF_GLUE.send(url);
-  debug(`Shutaf:: Sent URL: ${url}`);
-};
+    if (isActiveSubscriber(userMetadata)) {
+      debug(`Shutaf:: Disable...`);
+      return;
+    }
+
+    setInterval(() => {
+      sendMessage(ShutafMessageType.PING, null);
+    }, PING_INTERVAL_IN_SEC * 1000);
+
+    debug(`Shutaf:: init...`);
+    const url: string = browserWindow().location.href;
+    sendMessage(ShutafMessageType.GENERATE_AFFILIATE_LINK, url);
+
+    onHrefChange((href) => {
+      debug(`Shutaf:: URL changed...`);
+      sendMessage(ShutafMessageType.GENERATE_AFFILIATE_LINK, url);
+      debug(`Shutaf:: URL changed... Done`);
+    });
+    debug(`Shutaf:: init... Done`);
+  } catch (error) {
+    logError(error);
+  }
+})();
 
 const isActiveSubscriptionStatus = (status: string): boolean => status === "active" || status === "trialing";
 
@@ -29,32 +52,3 @@ const isActiveSubscriber = (userMetadata: any): boolean => {
   const paddle_subscription_id = userMetadata?.paddle_subscription_id;
   return isActiveSubscriptionStatus(subscription_status) && hasPaddleSubscriptionId(paddle_subscription_id);
 };
-
-(async () => {
-  try {
-    await authStoreReady().then((store) => {
-      debug(`Auth Content Script:: initializing.... Auth store is ready!`, store);
-    });
-
-    const { user } = useAuthStore.getState();
-    const userMetadata = user?.user_metadata;
-
-    if (isActiveSubscriber(userMetadata)) {
-      debug(`Shutaf:: Disable...`);
-      return;
-    }
-
-    setupPing();
-    debug(`Shutaf:: init...`);
-    const url: string = browserWindow().location.href;
-    storeAndSendUrl(url);
-    onHrefChange((href) => {
-      debug(`Shutaf:: URL changed...`);
-      storeAndSendUrl(href);
-      debug(`Shutaf:: URL changed... Done`);
-    });
-    debug(`Shutaf:: init... Done`);
-  } catch (error) {
-    logError(error);
-  }
-})();

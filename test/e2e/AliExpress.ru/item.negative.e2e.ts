acb4a7e346e3be9644ddd0a236c3f6f5d4ca2346
Chen Osipov <eobender@gmail.com>
2024-12-20 16:14:15 -0800

    update content-script-shutaf.tsx, item.negative.e2e.ts and playwright.config.ts

--- test/e2e/AliExpress.ru/item.negative.e2e.ts
+++ test/e2e/AliExpress.ru/item.negative.e2e.ts
@@ -17,6 +17,7 @@ test.describe("AliExpress.ru", () => {
       echo(url);
       await page.mouse.wheel(0, 100);
       await page.waitForSelector(SAFE_DEAL_BOX);
+      await page.click("[class*=ShipToHeaderItem_GeoTooltip__mapGeoButton]");
       const itemValue = await page.getAttribute("[data-sd-item]", "data-sd-item");
       await expect(itemValue).toBe(productId);
 
@@ -25,7 +26,7 @@ test.describe("AliExpress.ru", () => {
       await expect(sdBox.locator("text=Unrecommended")).toBeVisible();
       await expect(sdBox.locator("text=Reviews deficiency or poor reviews")).toBeVisible();
       await expect(sdBox.locator("text=Many Customers are dissatisfied with the store")).toBeVisible();
-      await expect(sdBox.locator("text=The price")).toBeVisible();
+      await expect(sdBox.locator("text=price")).toBeVisible();
     });
   });
 });

0fddd720df433f99f28ce199bc826851fe8bd1dc
chenreuven <chen.rvn@gmail.com>
2025-01-12 23:24:30 +0200

    fix

--- src/e-commerce/engine/stores/ebay/rules/return-policy/__tests__/rule-return-policy-algorithm.test.ts
+++ src/e-commerce/engine/stores/ebay/rules/return-policy/__tests__/rule-return-policy-algorithm.test.ts
@@ -1,10 +1,10 @@
 import BONUS_POINTS from "../../../../../../../constants/rule-bonus-value";
-import { calculateReturnPolicyValueAlgorhtim } from "../rule-return-policy-algorithm";
+import { calculateReturnPolicyValueAlgorithm } from "../rule-return-policy-algorithm";
 
 it("calculateShopOpenYearValueAlgorhtim", () => {
-  expect(calculateReturnPolicyValueAlgorhtim(undefined)).toBe(0);
-  expect(calculateReturnPolicyValueAlgorhtim("buyer pays")).toBe(BONUS_POINTS.NONE);
-  expect(calculateReturnPolicyValueAlgorhtim("20 days")).toBe(BONUS_POINTS.SEVEN);
-  expect(calculateReturnPolicyValueAlgorhtim("Free 30 day returns")).toBe(BONUS_POINTS.TEN);
-  expect(calculateReturnPolicyValueAlgorhtim("not accept returns")).toBe(BONUS_POINTS.NEGATIVE_SMALL);
+  expect(calculateReturnPolicyValueAlgorithm(undefined)).toBe(0);
+  expect(calculateReturnPolicyValueAlgorithm("buyer pays")).toBe(BONUS_POINTS.THREE);
+  expect(calculateReturnPolicyValueAlgorithm("20 days")).toBe(BONUS_POINTS.SEVEN);
+  expect(calculateReturnPolicyValueAlgorithm("Free 30 day returns")).toBe(BONUS_POINTS.TEN);
+  expect(calculateReturnPolicyValueAlgorithm("not accept returns")).toBe(BONUS_POINTS.NEGATIVE_SMALL);
 });

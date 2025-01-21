21105edaeca4961629dcb245479cdec9c21f91b4
chenreuven <chen.rvn@gmail.com>
2025-01-21 20:57:33 +0200

    fix auth

--- src/constants/api-params.ts
+++ src/constants/api-params.ts
@@ -1,3 +1,3 @@
 export const API_URL = "https://api.joinsafedeal.com";
 export const SHOPPING_URL = "https://joinsafedeal.com/shopping?mode=ext";
-export const EXTENSION_AUTH_PATH = "https://joinsafedeal.com/accounts/authorize/extension";
+export const EXTENSION_AUTH_PATH = "https://www.joinsafedeal.com/accounts/authorize/extension";

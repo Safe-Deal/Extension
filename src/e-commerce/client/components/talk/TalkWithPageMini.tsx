db57136ec7987e0af3ff012f3e19f77516992334
Chen Osipov <eobender@gmail.com>
2025-02-13 07:54:30 -0800

    chore: update VS Code test settings

---
+++ src/e-commerce/client/components/talk/TalkWithPageMini.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { Button, Dialog } from "@mui/material";
+import { useAuthStore } from "../../../../store/AuthState";
+import { t } from "../../../../constants/messages";
+import { Z_INDEX_MAX } from "../constants";
+import ProductChat from "../product/ProductChat";
+
+interface TalkWithPageMiniProps {
+  product: any;
+}
+
+const TalkWithPageMini: React.FC<TalkWithPageMiniProps> = ({ product }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const { session } = useAuthStore();
+
+  const handleOpen = () => {
+    setIsOpen(true);
+  };
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={handleOpen}
+        size="medium"
+        sx={{
+          minWidth: "120px",
+          textTransform: "none",
+          fontWeight: "bold"
+        }}
+      >
+        {t("talk_with_page")}
+      </Button>
+
+      <Dialog
+        open={isOpen}
+        onClose={handleClose}
+        fullWidth
+        maxWidth="md"
+        PaperProps={{
+          sx: {
+            height: "80vh",
+            maxHeight: "80vh"
+          }
+        }}
+        sx={{ zIndex: Z_INDEX_MAX }}
+      >
+        <ProductChat product={product} />
+      </Dialog>
+    </>
+  );
+};
+
+export default TalkWithPageMini;

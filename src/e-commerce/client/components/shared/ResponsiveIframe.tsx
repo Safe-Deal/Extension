9a1d1c8b02874a31dd38942f25af1468c4ca77a6
Chen Osipov <eobender@gmail.com>
2025-02-28 09:55:11 -0800

    refactor: clean up code and improve iframe and state management

--- src/e-commerce/client/components/shared/ResponsiveIframe.tsx
+++ src/e-commerce/client/components/shared/ResponsiveIframe.tsx
@@ -1,6 +1,5 @@
-import React from "react";
-import { forwardRef } from "react";
+import React, { forwardRef } from "react";
 
 export const ResponsiveIframe = forwardRef<HTMLIFrameElement, { src: string; onLoad?: () => void }>((props, ref) => (
-  <iframe ref={ref} {...props} style={{ width: "100%", height: "100%", border: "none" }} />
+  <iframe ref={ref} {...props} style={{ width: "100%", height: "100%", border: "none" }} title=" " />
 ));
